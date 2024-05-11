573e4dbb0dfe764588a0b169d698f9ebd493e2f6
mintgyver <phototravel2020@gmail.com>
2024-05-11 09:41:34 +0900

    modified:   "Lv.1/2022 KAKAO BLIND RECRUITMENT_\354\213\240\352\263\240 \352\262\260\352\263\274 \353\260\233\352\270\260.py"

--- Lv.1/2022 KAKAO BLIND RECRUITMENT_신고 결과 받기.py
+++ Lv.1/2022 KAKAO BLIND RECRUITMENT_신고 결과 받기.py
@@ -1,22 +1,27 @@
+#각 유저는 한번에 한 명의 유저를 신고할 수 있습니다.
+#한 유저를 여러 번 신고할 수도 있지만, 동일한 유저에 대한 신고 횟수는 1회로 처리됩니다.
+#k번 이상 신고된 유저는 게시판 이용이 정지되며, 해당 유저를 신고한 모든 유저에게 정지 사실을 메일로 발송합니다.
+
+
 def solution(id_list, report, k):
     
     id_len = len(id_list)
-    warn = [0 for i in range(id_len)]
-    id_list_warn = []
-    answer = [0 for i in range(id_len)]
+    warn = [0 for i in range(id_len)]                   #경고 횟수 파악을 위한 리스트
+    id_list_warn = []                                   #경고 받은 아이디 리스트
+    answer = [0 for i in range(id_len)] 
     
-    report_set = list(set(report))
-    report_name = [i.split() for i in report_set]
+    report_set = list(set(report))                      #리포트 중복 제거를 위한 set
+    report_name = [i.split() for i in report_set]       #split을 통해 신고자와 신고대상자 분리
     
     for i in report_name : 
-        warn[id_list.index(i[1])] += 1
+        warn[id_list.index(i[1])] += 1                  #신고대상자 카운트
                         
     for i in range(id_len) : 
-        if warn[i] >= k :
+        if warn[i] >= k :                               #k번 이상이면 이름 기록
             id_list_warn.append(id_list[i])
         
     for i in report_name : 
-        if i[1] in id_list_warn :
+        if i[1] in id_list_warn :                       #기록된 이름 신고자 카운트 하기
             answer[id_list.index(i[0])] += 1
                             
     return answer
